1b2614c65a74afd34498348adc3a5c724293c795
Vishal Kotha <kumar.vishalreddy@gmail.com>
2019-12-15 17:32:25 +0530

    Commit all

---
+++ CleanArchDemoMVC/Controllers/CourseController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+
+namespace CleanArchDemoMVC.Controllers
+{
+    public class CourseController : Controller
+    {
+        private ICourseService _courseServ;
+        public CourseController(ICourseService courseServ)
+        {
+            _courseServ = courseServ;
+        }
+        public IActionResult Index()
+        {
+            CourseViewModel model = _courseServ.getCourses();
+            return View(model);
+        }
+    }
+}
